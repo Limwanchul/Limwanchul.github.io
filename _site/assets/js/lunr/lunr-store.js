af30100468c54ad0ad776f61d5708fb03f8d1e0f
Limwanchul <gop78@naver.com>
2021-03-18 23:28:35 +0900

    github.io 블로그 시작하기

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "github.io 블로그 시작하기",
+        "excerpt":"github.io 블로그 시작 생각만 해오던 블로그를 시작해 보려 한다. jekyll, markdown 등등 아직 배워야 할 게 너무 많다. 처음은 미숙하게 시작하지만, 점점 발전하는 개발자가 되보려한다. 나의 배움에 대해 꾸준히 기록해 나가는 나만의 기록장이 되었으면 한다. 블로그에는 뭐를? 이 블로그는 내가 성장해 나가는 모습을 남기기 위한 블로그이다. 개발자로서 사람으로서 발전해 나가는...","categories": ["Blog","github.io"],
+        "tags": ["Blog","github.io"],
+        "url": "/blog/github.io/first-post/",
+        "teaser": null
+      }]
